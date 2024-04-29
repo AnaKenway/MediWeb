832a5050fb1504f4c53609d1b7b957917bb6654d
AnaKenway <ancemilenkovic@gmail.com>
2024-04-29 19:30:07 +0200

    WIP; Creating User Account View Models and Views

---
+++ MediWeb/MediWeb/Models/UserAccountViewModels/PatientViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace MediWeb.Models;
+
+public class PatientViewModel : UserAccountViewModel
+{
+    public long PatientId { get; set; }
+    public string Jmbg { get; set; } = null!;
+    public Gender Gender {  get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string PhoneNumber { get; set; } = null!;
+
+}
---
+++ MediWeb/MediWeb/Models/UserAccountViewModels/UserAccountViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MediWeb.Models;
+
+public class UserAccountViewModel
+{
+    public UserAccountViewModel() 
+    { 
+        Email = string.Empty;
+        FirstName = string.Empty;
+        LastName = string.Empty;
+        Password = string.Empty;
+        ConfirmPassword = string.Empty;
+    }
+
+    public long UserAccountId { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+
+}
