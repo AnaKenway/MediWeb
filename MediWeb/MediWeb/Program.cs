2bc37cf7464da771cacd40780db566eae2a70289
AnaKenway <ancemilenkovic@gmail.com>
2024-05-04 11:59:45 +0200

    Registered Unit of Work and all created Services

--- MediWeb/MediWeb/Program.cs
+++ MediWeb/MediWeb/Program.cs
@@ -1,4 +1,5 @@
 using DataLayer;
+using MediWeb.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,11 +7,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Add services from Services project
+builder.Services.AddScoped<AdminService>();
+builder.Services.AddScoped<AppointmentService>();
+builder.Services.AddScoped<AppointmentSlotService>();
+builder.Services.AddScoped<ClinicService>();
+builder.Services.AddScoped<DoctorService>();
+builder.Services.AddScoped<DoctorWorksAtClinicService>();
+builder.Services.AddScoped<MedicalStaffService>();
+builder.Services.AddScoped<PatientService>();
+builder.Services.AddScoped<SpecializationService>();
+builder.Services.AddScoped<UserAccountService>();
+
 builder.Services.AddRazorComponents();
 
 //Add DBContext
 builder.Services.AddDbContext<MediWebContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+//Add Unit of Work
+builder.Services.AddScoped<UnitOfWork>();
 
 var app = builder.Build();
 
